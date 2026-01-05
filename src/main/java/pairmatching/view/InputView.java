c34325dacc97bcdef3deb9b98cfdce332dfa6837
yeo-li <parkseongyeol2110@gmail.com>
2026-01-05 16:27:58 +0900

    feat(InputView): 페어 매칭 과정, 레벨, 미션 입력 뷰 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -6,10 +6,26 @@ import pairmatching.util.Parser;
 public class InputView {
 
     public final static String INPUT_FUNCTION_MESSAGE = "기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
+    public final static String INPUT_PAIR_MATCHING_MESSAGE =
+        "#############################################\n"
+            + "과정: 백엔드 | 프론트엔드\n"
+            + "미션:\n"
+            + "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
+            + "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
+            + "  - 레벨3: \n"
+            + "  - 레벨4: 성능개선 | 배포\n"
+            + "  - 레벨5: \n"
+            + "############################################\n"
+            + "과정, 레벨, 미션을 선택하세요.\n"
+            + "ex) 백엔드, 레벨1, 자동차경주";
 
     public String inputFunction() {
         System.out.println(INPUT_FUNCTION_MESSAGE);
         return Parser.removeAllSpaces(Console.readLine());
     }
-    
+
+    public String inputPairMatching() {
+        System.out.println(INPUT_PAIR_MATCHING_MESSAGE);
+        return Parser.removeAllSpaces(Console.readLine());
+    }
 }
