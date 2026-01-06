7e4b531e154bb88df7cf0b184e7f4e8546e49559
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 16:19:54 +0900

    feat: OutputView 클래스 기본 로직 추가

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,18 @@
+package pairmatching.view;
+
+public class OutputView {
+
+    public void printInfo() {
+
+    }
+
+    public void printResult() {
+
+    }
+
+    public void printError(String message) {
+        System.out.println(message);
+    }
+
+
+}
