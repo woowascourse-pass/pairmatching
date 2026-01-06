e38d3e50eb6c838aa2010fb29a527fd6dc846780
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 18:22:14 +0900

    feat: PairMatchingController 기본 로직 추가

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,9 +1,13 @@
 package pairmatching.controller;
 
 import pairmatching.model.Crew;
+import pairmatching.model.Info;
 import pairmatching.service.PairMatchingService;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PairMatchingController {
     private final InputView inputView;
@@ -18,44 +22,42 @@ public class PairMatchingController {
 
     public void run() {
         // 크루 파일 읽기
-        Crew backendCrew = pairMatchingService.loadBackendCrew();
-        Crew frontendCrew = pairMatchingService.loadFrontendCrew();
+        final Crew backendCrew = pairMatchingService.loadBackendCrew();
+        final Crew frontendCrew = pairMatchingService.loadFrontendCrew();
+        Map<Info, Crew> pairMatchingStorage = new HashMap<>();
         while (true) {
             // 기능 선택
             String command = readCommandUntilValid();
-            Crew pairMatchingResult;
             // 페어 매칭
             if (command.equals("1")) {
                 outputView.printInfo();
-                // 매칭 정보 존재
-                if () {
+                // 과정, 레벨, 미션 입력
+                Info info = readInfoUntilValid();
+                // 매칭 정보 존재 여부 판단
+                if (pairMatchingStorage.containsKey(info)) {
                     String whether = readWhetherUntilValid();
-
                     // 재매칭 안함
-                    if (!whether) {
-                        // 다시 기능 선택으로 돌아가기
+                    if (whether.equals("아니오")) {
                         continue;
                     }
                 }
-
-                // 과정, 레벨, 미션 입력
-                String info = readInfoUntilValid();
-                pairMatchingResult = pairMatchingService.pairMatching();
-                outputView.printPairMatchingResult(pairMatchingResult);
+                Crew pairMatchingResult = pairMatchingService.pairMatching(info.course(), backendCrew, frontendCrew);
+                pairMatchingStorage.put(info, pairMatchingResult);
+                outputView.printPairMatchingResult(pairMatchingStorage.get(info));
                 continue;
             }
             // 페어 조회
             if (command.equals("2")) {
                 outputView.printInfo();
                 // 과정, 레벨, 미션 입력
-                String info = readInfoUntilValid();
-                outputView.printPairMatchingResult(pairMatchingResult);
+                Info info = readInfoUntilValid();
+                outputView.printPairMatchingResult(pairMatchingStorage.get(info));
                 continue;
             }
             // 페어 초기화
             if (command.equals("3")) {
                 outputView.printClear();
-                pairMatchingResult.clear();
+                pairMatchingStorage.clear();
                 continue;
             }
             // 종료
@@ -76,7 +78,7 @@ public class PairMatchingController {
         }
     }
 
-    private String readInfoUntilValid() {
+    private Info readInfoUntilValid() {
         while (true) {
             try {
                 return inputView.readInfo();
@@ -89,7 +91,7 @@ public class PairMatchingController {
     private String readWhetherUntilValid() {
         while (true) {
             try {
-                return inputView.readCommand();
+                return inputView.readWhether();
             } catch (IllegalArgumentException e) {
                 outputView.printError(e.getMessage());
             }
