832c515232fbc771133b93bc3d1fb0bd5cb84e0b
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 16:17:05 +0900

    feat: 프로그램 진입점 로직 추가

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,25 @@
 package pairmatching;
 
+
+import pairmatching.controller.PairMatchingController;
+import pairmatching.repository.CrewRepository;
+import pairmatching.service.PairMatchingService;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
+    private static final String BACKEND_CREW_FILE_PATH = "holiday.md";
+    private static final String FRONTEND_CREW_FILE_PATH = "holiday.md";
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CrewRepository backendCrewRepository = new CrewRepository(BACKEND_CREW_FILE_PATH);
+        CrewRepository frontendCrewRepository = new CrewRepository(FRONTEND_CREW_FILE_PATH);
+        PairMatchingService pairMatchingService = new PairMatchingService(backendCrewRepository, frontendCrewRepository);
+
+        PairMatchingController pairMatchingController = new PairMatchingController(inputView, outputView, pairMatchingService);
+        pairMatchingController.run();
     }
 }
