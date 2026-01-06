88c2d8df9f9ab7c87f93d55d8896d0c4abdbdbd4
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 16:17:36 +0900

    feat: Crew 클래스 추가

---
+++ src/main/java/pairmatching/model/Crew.java
@@ -0,0 +1,13 @@
+package pairmatching.model;
+
+import java.util.List;
+
+public class Crew {
+    private List<CrewMember> crewMembers;
+
+    public Crew(List<CrewMember> crewMembers) {
+        this.crewMembers = crewMembers;
+    }
+
+
+}
