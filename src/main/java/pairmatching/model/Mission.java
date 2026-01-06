8df82ff17e3d186919051d268a5e58d58c47be4f
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 18:20:55 +0900

    feat: Mission 클래스 기본 로직 추가

--- src/main/java/pairmatching/model/Mission.java
+++ src/main/java/pairmatching/model/Mission.java
@@ -1,5 +1,7 @@
 package pairmatching.model;
 
+import java.util.Arrays;
+
 public enum Mission {
     RacingCar(Level.LEVEL1, "자동차경주"),
     Lotto(Level.LEVEL1, "로또"),
@@ -8,8 +10,7 @@ public enum Mission {
     ShoppingBasket(Level.LEVEL2, "장바구니"),
     Payment(Level.LEVEL2, "결제"),
     SubwayMap(Level.LEVEL2, "지하철노선도"),
-
-    // none
+// none
 
     Improvement(Level.LEVEL4, "성능개선"),
     Distribution(Level.LEVEL4, "배포"),
@@ -24,5 +25,20 @@ public enum Mission {
         this.name = name;
     }
 
+    public Level getLevel() {
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Mission find(Level level,String missionName) {
+        return Arrays.stream(values())
+                .filter(mission -> mission.level.equals(level)
+                        && mission.name.equals(missionName))
+                .findFirst()
+                .orElse(None);
+    }
 
 }
