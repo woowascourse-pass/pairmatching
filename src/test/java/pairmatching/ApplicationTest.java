308a17b755b17b6843fb9f64e4e7115bdaf71c5e
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 19:27:10 +0900

    test: Application Test 케이스 추가

--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -25,6 +25,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 홀수_인원_페어_매칭() {
+        assertShuffleTest(
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "Q");
+                    assertThat(output()).contains("태웅2 : 백호", "치수 : 태섭 : 포비");
+                },
+                Arrays.asList("태웅2", "백호", "치수", "태섭", "포비")
+        );
+    }
+
+    @Test
     void 없는_미션에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
