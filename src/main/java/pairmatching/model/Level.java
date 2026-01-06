7c381bd8ac607faf57fe48d0efe0abb21b2a5efe
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 18:21:03 +0900

    feat: Level 클래스 기본 로직 추가

--- src/main/java/pairmatching/model/Level.java
+++ src/main/java/pairmatching/model/Level.java
@@ -13,5 +13,18 @@ public enum Level {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public static Level from(String levelName) {
+        String trimmed = levelName.trim();
+        for (Level level : Level.values()) {
+            if (trimmed.equals(level.getName())) {
+                return level;
+            }
+        }
+
+        throw new IllegalArgumentException("없는 레벨 ㅜㅜ");
+    }
 }
