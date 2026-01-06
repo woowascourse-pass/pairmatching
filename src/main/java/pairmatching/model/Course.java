e4d9ae2b86d8676f561e71ca837350ef69f30685
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 18:09:13 +0900

    feat: Course 클래스 기본 로직 추가

--- src/main/java/pairmatching/model/Course.java
+++ src/main/java/pairmatching/model/Course.java
@@ -4,11 +4,24 @@ public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
-    private String name;
+    private final String name;
 
     Course(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public static Course from(String name) {
+        String trimmed = name.trim();
+        for (Course course : Course.values()) {
+            if (trimmed.equals(course.getName())) {
+                return course;
+            }
+        }
+
+        throw new IllegalArgumentException("없는 코스 ㅜㅜ");
+    }
 }
