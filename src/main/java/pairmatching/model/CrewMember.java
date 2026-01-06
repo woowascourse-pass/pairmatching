aff3f01bdb43ac91a57c17ead886448eaa22c37f
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 16:17:44 +0900

    feat: CrewMember 클래스 추가

---
+++ src/main/java/pairmatching/model/CrewMember.java
@@ -0,0 +1,11 @@
+package pairmatching.model;
+
+public class CrewMember {
+    private final Course course;
+    private final String name;
+
+    public CrewMember(Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+}
