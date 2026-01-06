12bce6234fee6cf20aba1aa972751b6e319fc04f
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 19:25:40 +0900

    refactor: Info 클래스 필드 변경

--- src/main/java/pairmatching/model/Info.java
+++ src/main/java/pairmatching/model/Info.java
@@ -2,13 +2,11 @@ package pairmatching.model;
 
 public class Info {
     private final Course course;
-    private final Level level;
-    private final String missionName;
+    private final Mission mission;
 
-    public Info(Course course, Level level, String missionName) {
+    public Info(Course course, Mission mission) {
         this.course = course;
-        this.level = level;
-        this.missionName = missionName;
+        this.mission = mission;
     }
-
 }
+
