79017d7c9044ec60cbb269ca8298723dc7b03c53
Junha Park <poiu7825@pusan.ac.kr>
2026-01-06 16:19:00 +0900

    feat: Info 클래스 추가

---
+++ src/main/java/pairmatching/model/Info.java
@@ -0,0 +1,14 @@
+package pairmatching.model;
+
+public class Info {
+    private final Course course;
+    private final Level level;
+    private final String missionName;
+
+    public Info(Course course, Level level, String missionName) {
+        this.course = course;
+        this.level = level;
+        this.missionName = missionName;
+    }
+
+}
